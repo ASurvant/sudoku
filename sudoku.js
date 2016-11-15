7cc31a3548b30da4cfee2c78f02580a2d167128a
Anthony Survant <anthonysurvant@gmail.com>
2016-11-15 16:21:04 -0500

    fixing flexblox bugs

--- sudoku.js
+++ sudoku.js
@@ -1,9 +1,22 @@
 $(function() {
   $('div').mouseenter(function() {
-    $(this).css("background-color", "yellow");
+      $(this).css("background-color", "yellow");
+
   });
 
   $('div').mouseleave(function() {
-    $(this).css("background-color", "white");
+        $(this).css("background-color", "white");
   });
+
+  $('div').click(function() {
+    $(this).toggleClass("selected");
+    // $('#numbers-container').toggleClass("selected");
+  })
+
+  // $('div').click(function() {
+  //   $('#numbers-container').css("background-color", "yellow");
+  //
+  //   $(this).click(function() {
+  //   });
+  // });
 });
